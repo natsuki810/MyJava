d771a6a9ef044f3e29acfe4171063db788745782
natsuki <144816558+natsuki810@users.noreply.github.com>
2024-05-20 22:15:52 +0900

    java_文字列学習

---
+++ Lesson4_2.java
@@ -0,0 +1,10 @@
+class Lesson4_2 {
+    public static void main(String[] args) {
+        double var01 = 1.23456789;
+        float var02 = 1.234F;
+
+        System.out.println(var01);
+        System.out.println(var02);
+    }
+    
+}
