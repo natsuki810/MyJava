02b5b1bd2d82ec14688d1474c7f019a1507d1cbb
natsuki <144816558+natsuki810@users.noreply.github.com>
2024-05-26 15:19:26 +0900

    java_2次元配列

--- Lesson5_3.java
+++ Lesson5_3.java
@@ -5,6 +5,7 @@ class Lesson5_3 {
         System.out.println(arr[0][0]);
         System.out.println(arr[0][1]);
         System.out.println(arr[1][0]);
+        System.out.println(arr[1][1]);
 
     }
 }
