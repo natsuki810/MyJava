a62b1be261f04a8971f98e3089163b2b9230c146
natsuki <144816558+natsuki810@users.noreply.github.com>
2024-05-26 15:30:11 +0900

    java_演算子

---
+++ Lesson6_2.java
@@ -0,0 +1,12 @@
+public class Lesson6_2 {
+    public static void main(String[] args){
+        int x = 10;
+        int y = 2;
+        int z = 10;
+
+        System.out.println(x >= y);
+        System.out.println(x <= y);
+        System.out.println(x <= z);
+    }
+    
+}
---
+++ Lesson6_3.java
@@ -0,0 +1,9 @@
+public class Lesson6_3 {
+    public static void main(String[] args) {
+       int x = 10;
+       int y = 2;
+       
+       System.out.println(x == y);
+       System.out.println(x != y);
+    }
+}
