7f53cafb05f9dcf45b5964f572f57431d0b2fc76
natsuki <144816558+natsuki810@users.noreply.github.com>
2024-05-26 16:13:15 +0900

    java_繰り返し

---
+++ Lesson11.java
@@ -0,0 +1,11 @@
+public class Lesson11 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 4; i ++) {
+            if(i==3){
+                //処理を終わらせる
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
---
+++ Lesson11_3.java
@@ -0,0 +1,9 @@
+public class Lesson11_3 {
+    public static void main(String[] args) {
+        for(int i =0; i <= 2; i ++){
+            for(int j=0; j <= 2; j ++){
+                System.out.println( i + "-" + j);
+            }
+        }
+    }
+}
