e34cbcb751c26c9f8c93fa4359dad211d714287a
natsuki <144816558+natsuki810@users.noreply.github.com>
2024-05-26 15:39:13 +0900

    java_複合代入演算子

---
+++ Lesson8.java
@@ -0,0 +1,10 @@
+public class Lesson8 {
+    public static void main(String[] args) {
+        int x = 8;
+        int y = 12;
+        int z = 20;
+
+        System.out.println(x += 10);
+        System.out.println(z += y);
+    }
+}
