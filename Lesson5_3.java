cc916a3628d65b7678bee803737689313524737c
natsuki <144816558+natsuki810@users.noreply.github.com>
2024-05-26 15:12:39 +0900

    Java_配列

---
+++ Lesson5_3.java
@@ -0,0 +1,10 @@
+class Lesson5_3 {
+    public static void main(String[] args) {
+        String[][] arr = {{"sato","suzuki"},{"takahashi","tanaka"}};
+
+        System.out.println(arr[0][0]);
+        System.out.println(arr[0][1]);
+        System.out.println(arr[1][0]);
+
+    }
+}
