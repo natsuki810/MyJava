b1d2c04bb5d04d12c959c64f55abba60e936c259
natsuki <144816558+natsuki810@users.noreply.github.com>
2024-05-26 15:42:32 +0900

    java_インクリメントデクリメント

---
+++ Lesson9.java
@@ -0,0 +1,11 @@
+public class Lesson9 {
+    public static void main(String[] args) {
+        int x = 8;
+        int y = 8;
+        x++;
+        y--;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
